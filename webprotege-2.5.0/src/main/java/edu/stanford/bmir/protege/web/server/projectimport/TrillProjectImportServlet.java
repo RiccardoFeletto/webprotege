1b19f857eda3a3238e87b41ea141f4b4631879ec
Riccardo Feletto <riccardo.feletto@gmai.com>
2016-05-19 16:26:43 +0200

    Collegamento progetto ad ontologia e apertura progetto appena creato

--- webprotege-2.5.0/src/main/java/edu/stanford/bmir/protege/web/server/projectimport/TrillProjectImportServlet.java
+++ webprotege-2.5.0/src/main/java/edu/stanford/bmir/protege/web/server/projectimport/TrillProjectImportServlet.java
@@ -1,12 +1,12 @@
 package edu.stanford.bmir.protege.web.server.projectimport;
 
 import edu.stanford.bmir.protege.web.client.rpc.data.DocumentId;
+import edu.stanford.bmir.protege.web.client.rpc.data.FileUploadResponseAttributes;
 import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIProject;
 import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIProjectCache;
 import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIProjectDocumentStore;
 import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIProjectManager;
 import edu.stanford.bmir.protege.web.client.rpc.data.NewProjectSettings;
-import edu.stanford.bmir.protege.web.client.rpc.data.NotSignedInException;
 import edu.stanford.bmir.protege.web.client.rpc.data.ProjectType;
 import edu.stanford.bmir.protege.web.server.MetaProjectManager;
 import edu.stanford.bmir.protege.web.server.ProjectManagerServiceImpl;
@@ -20,6 +20,7 @@ import edu.stanford.smi.protege.server.metaproject.ProjectInstance;
 import edu.stanford.bmir.protege.web.client.rpc.ProjectManagerService;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -27,6 +28,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -44,7 +47,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gwt.core.client.GWT;
+import org.apache.commons.io.IOUtils;
 
 /**
  * Servlet implementation class TrillProjectImportServlet
@@ -53,30 +56,27 @@ public class TrillProjectImportServlet extends HttpServlet {
        
    @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	String contenuto = request.getParameter("ontology");
+	
+	String contenuto = request.getParameter("ontology");	//Prelevo l'ontologia ricevuta da trill
 
-	String url = "http://localhost:8080/webprotege-2.5.0/webprotege/submitfile";
+	String url = "http://localhost:8080/webprotege-2.5.0/webprotege/submitfile"; //Indirizzo della servlet per l'upload dell'ontologia
 	String charset = "UTF-8";
 	String boundary = Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
-	String CRLF = "\r\n"; // Line separator required by multipart/form-data.
+	String CRLF = "\r\n"; 											// Line separator required by multipart/form-data.
 	
-	String paramPrName = "progettoprova";
-	String paramPrDesc= "progetto prova descrizione";
-	String paramPrType = "OWL Project";
+	String tDir = System.getProperty("java.io.tmpdir"); //Path della cartella tmp del server
 	
 	byte[] buffer = new byte[1024];
 	
-	String tDir = System.getProperty("java.io.tmpdir");
+	File tmpZip = File.createTempFile("tmpfilezip", ".zip"); //File zip che contiene l'ontologia
+	File tmpOWL = new File(tDir + "/root-ontology.owl");	//File owl che contiene l'ontologia
 	
-	File tmpZip = File.createTempFile("tmpfilezip", ".zip");
-	File tmpOWL = new File(tDir + "/root-ontology.owl");
-	
-	BufferedWriter bw = new BufferedWriter(new FileWriter(tmpOWL));
+	BufferedWriter bw = new BufferedWriter(new FileWriter(tmpOWL)); //Scrivo il testo dell'ontologia all'interno del file
 	bw.write(contenuto);
 	bw.close();
 	
+	//Comprimo l'ontologia nel file zip
 	FileOutputStream fos = new FileOutputStream(tmpZip.getPath());
-    //ByteArrayOutputStream baos = new ByteArrayOutputStream();
     ZipOutputStream zos = new ZipOutputStream(fos);
     
     ZipEntry entry = new ZipEntry(tmpOWL.getName());
@@ -90,10 +90,9 @@ public class TrillProjectImportServlet extends HttpServlet {
 
 	in.close();
 	zos.closeEntry();
-   
-	//remember close it
 	zos.close();
     
+	//Creo la connesione per inviare il file tramite post
     URLConnection connection = new URL(url).openConnection();
     connection.setDoOutput(true);
     connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
@@ -101,53 +100,50 @@ public class TrillProjectImportServlet extends HttpServlet {
     try (
     	OutputStream output = connection.getOutputStream();
         PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
-    ){
-    	// Send normal param.
-        writer.append("--" + boundary).append(CRLF);
-        writer.append("Content-Disposition: form-data; name=\"projectname\"").append(CRLF);
-        writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
-        writer.append(CRLF).append(paramPrName).append(CRLF).flush();
-        
-        writer.append("--" + boundary).append(CRLF);
-        writer.append("Content-Disposition: form-data; name=\"projectdescription\"").append(CRLF);
-        writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(tmpZip.getName())).append(CRLF);
-        writer.append("Content-Transfer-Encoding: binary").append(CRLF);
-        
-        writer.append("--" + boundary).append(CRLF);
-        writer.append("Content-Disposition: form-data; name=\"projecttype\"").append(CRLF);
-        writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
-        writer.append(CRLF).append(paramPrType).append(CRLF).flush();
-        
-    	//Send zip file.
+    ){        
+    	//Invio il file zip
         writer.append("--" + boundary).append(CRLF);
         writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + tmpZip.getName() + "\"").append(CRLF);
-        writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF); // Text file itself must be saved in this charset!
+        writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
         writer.append(CRLF).flush();
         Files.copy(tmpZip.toPath(), output);
-        output.flush(); // Important before continuing with writer!
-        writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+        output.flush();
+        writer.append(CRLF).flush();
         
         //End of multipart/form-data.
         writer.append("--" + boundary + "--").append(CRLF).flush();
         
-        ProjectType prtype = new ProjectType(paramPrType);
+        //Prelevo la risposta dell'upload dell'ontologia
+        InputStream inn = connection.getInputStream();
+        String encoding = connection.getContentEncoding();
+        encoding = encoding == null ? "UTF-8" : encoding;
+        String body = IOUtils.toString(inn, encoding);
         
-        NewProjectSettings newProjectSettings = new NewProjectSettings(UserId.getGuest(),paramPrName,paramPrDesc,prtype);  
+        //TimeStamp
+        String timeStampName = new SimpleDateFormat("HH.mm.ss").format(new Date());
+        String timeStampDesc = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         
+        ProjectType prtype = new ProjectType("OWL Project");  			     //Project type
+        String paramPrName = "project_tmp_" + timeStampName;			     //Project name
+    	String paramPrDesc= "temp project " + timeStampDesc;				 //Project desc
+        
+        String uploadid = body.split(":")[2];
+        uploadid = uploadid.substring(uploadid.indexOf('"')+1, uploadid.lastIndexOf('"')).trim();
+        DocumentId docid = new DocumentId(uploadid);		  //Id file ontologia
+        
+        //Creo il progetto
+        NewProjectSettings newProjectSettings = new NewProjectSettings(UserId.getGuest(),paramPrName,paramPrDesc,prtype,docid);  
         OWLAPIProjectCache pc = new OWLAPIProjectCache();
-        
         OWLAPIProject pro = pc.getProject(newProjectSettings);
-        
         MetaProjectManager mpm = MetaProjectManager.getManager();
-        
         mpm.registerProject(pro.getProjectId(), newProjectSettings);
         
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><head><title>prova</title></head><body>");
-        out.println(tmpZip.toPath()+"<br>"+ pro.getProjectId()+"<br>");
-        out.println(((HttpURLConnection) connection).getResponseMessage());
-        out.println("<br></body></html>");
+        //Construisco url per aprire il progetto
+        String proid = pro.getProjectId().toString();
+        proid = proid.substring(proid.indexOf('{')+1, proid.lastIndexOf('}')).trim();
+        String redurl = "http://localhost:8080/webprotege-2.5.0/#Edit:projectId="+proid;
+        
+        response.sendRedirect(redurl); //Redirect per aprire il progetto
     }
    }
 }  
